292bccf2492b30a3d9d6b7dec5f7b5b59172bc55
David van der Spek <david.vanderspek@flyrlabs.com>
2024-09-25 17:07:57 +0200

    last changes

--- init.lua
+++ init.lua
@@ -945,6 +945,14 @@ require('lazy').setup({
     cmd = 'Git',
   },
   { 'tpope/vim-rhubarb' },
+  {
+    'cuducos/yaml.nvim',
+    ft = { 'yaml' }, -- optional
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
